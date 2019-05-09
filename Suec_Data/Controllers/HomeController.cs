a0823012d7391215aa1681d48ce664d51bcb6ebb
suec <suec@bqool.com>
2019-05-09 14:26:33 +0800

    變數修改

--- Suec_Data/Controllers/HomeController.cs
+++ Suec_Data/Controllers/HomeController.cs
@@ -16,8 +16,8 @@ namespace Suec_Data.Controllers
     {
         public ActionResult Index()
         {
-            string filepath = Server.MapPath("~/App_Data/data.json");
-            string json = System.IO.File.ReadAllText(filepath);
+            string filePath = Server.MapPath("~/App_Data/data.json");
+            string json = System.IO.File.ReadAllText(filePath);
 
             List<TestModel> list = JsonConvert.DeserializeObject<List<TestModel>>(json);
             foreach (var item in list)
@@ -28,28 +28,28 @@ namespace Suec_Data.Controllers
                 }
                 else
                 {
-                    bool resultPrice = decimal.TryParse(item.Price, out decimal typetest);
+                    bool resultPrice = decimal.TryParse(item.Price, out decimal typeTest);
                     if (resultPrice == true)
                     {
                         switch (item.Locale)
                         {
                             case "US":
-                                item.Price = string.Format(new CultureInfo("en-US"), "{0:C}", typetest);
+                                item.Price = string.Format(new CultureInfo("en-US"), "{0:C}", typeTest);
                                 break;
                             case "CA":
-                                item.Price = string.Format(new CultureInfo("en-CA"), "{0:C}", typetest);
+                                item.Price = string.Format(new CultureInfo("en-CA"), "{0:C}", typeTest);
                                 break;
                             case "DE":
-                                item.Price = string.Format(new CultureInfo("de-DE"), "{0:C}", typetest);
+                                item.Price = string.Format(new CultureInfo("de-DE"), "{0:C}", typeTest);
                                 break;
                             case "ES":
-                                item.Price = string.Format(new CultureInfo("es-ES"), "{0:C}", typetest);
+                                item.Price = string.Format(new CultureInfo("es-ES"), "{0:C}", typeTest);
                                 break;
                             case "FR":
-                                item.Price = string.Format(new CultureInfo("fr-FR"), "{0:C}", typetest);
+                                item.Price = string.Format(new CultureInfo("fr-FR"), "{0:C}", typeTest);
                                 break;
                             case "JP":
-                                item.Price = string.Format(new CultureInfo("ja-JP"), "{0:C}", typetest);
+                                item.Price = string.Format(new CultureInfo("ja-JP"), "{0:C}", typeTest);
                                 break;
                         }
                     }
@@ -58,28 +58,28 @@ namespace Suec_Data.Controllers
                 {
                     item.Promote_Price = "-";
                 }
-                bool resultPromote = decimal.TryParse(item.Promote_Price, out decimal typechange);
+                bool resultPromote = decimal.TryParse(item.Promote_Price, out decimal typeChange);
                 if (resultPromote == true)
                 {
                     switch (item.Locale)
                     {
                         case "US":
-                            item.Promote_Price = string.Format(new CultureInfo("en-US"), "{0:C}", typechange);
+                            item.Promote_Price = string.Format(new CultureInfo("en-US"), "{0:C}", typeChange);
                             break;
                         case "CA":
-                            item.Promote_Price = string.Format(new CultureInfo("en-CA"), "{0:C}", typechange);
+                            item.Promote_Price = string.Format(new CultureInfo("en-CA"), "{0:C}", typeChange);
                             break;
                         case "DE":
-                            item.Promote_Price = string.Format(new CultureInfo("de-DE"), "{0:C}", typechange);
+                            item.Promote_Price = string.Format(new CultureInfo("de-DE"), "{0:C}", typeChange);
                             break;
                         case "ES":
-                            item.Promote_Price = string.Format(new CultureInfo("es-ES"), "{0:C}", typechange);
+                            item.Promote_Price = string.Format(new CultureInfo("es-ES"), "{0:C}", typeChange);
                             break;
                         case "FR":
-                            item.Promote_Price = string.Format(new CultureInfo("fr-FR"), "{0:C}", typechange);
+                            item.Promote_Price = string.Format(new CultureInfo("fr-FR"), "{0:C}", typeChange);
                             break;
                         case "JP":
-                            item.Promote_Price = string.Format(new CultureInfo("ja-JP"), "{0:C}", typechange);
+                            item.Promote_Price = string.Format(new CultureInfo("ja-JP"), "{0:C}", typeChange);
                             break;
                     }
                 }
